10166d71740ccec6b7ea0d7a3b295922d4b6a010
Misho <meantimetofailure@gmail.com>
2017-06-16 13:18:26 -0500

    Moved TODO list

--- ytdgui.py
+++ ytdgui.py
@@ -159,18 +159,3 @@ if __name__ == '__main__':
     window = GUI(root)
     root.mainloop()
 
-'''
-TODO
--replace basic button with menu bar that has the 3 different modes, and an about section
--add more options
--make the basic button do something
--make the program startup faster
--make black boxes go away
--cry
--make the status bar do something
--make icon better
--support linux maybe?
-'''
-'''
-command to build program into exe: pyinstaller --onefile --icon=moon.ico --windowed ytdgui.py
-'''
